69520e56991ad5a1d739d7393861e648792ecdcf
Michael Calkins <michael@clouddueling.com>
2013-05-24 16:26:57 -0600

    Update directives.js

--- directives.js
+++ directives.js
@@ -1,47 +1,45 @@
-directives.directive('ngPopup', function(PopupService){
+directives.directive('ngPopup', function(Popup){
     return {
-        restrict: 'A',
-        link: function postLink(scope, element, attrs) {
-            var ngPopupUrl= attrs['ngPopupUrl'];
-            // Could have custom or boostrap modal options here
-            var popupOptions = {};
-            element.bind( "click", function()
-            {
-                PopupService.load(ngPopupUrl, scope, popupOptions);
-            });
-        }
-    };
+		restrict: 'A',
+		link: function postLink(scope, element, attrs) {
+			var ngPopupUrl = attrs['ngPopup'];
+			// Could have custom or boostrap modal options here
+			var popupOptions = {};
+
+			element.bind( "click", function(){
+				Popup.load(ngPopupUrl, scope, popupOptions);
+			});
+		}
+	};
 });
 
-directives.directive('ngConfirm', function(PopupService) {
-    return {
-        restrict: 'E',
-        link: function postLink(scope, element, attrs) {
-            // Could have custom or boostrap modal options here
-            var popupOptions = {};
-            element.bind("click", function()
-            {
-                PopupService.confirm(attrs["title"], attrs["actionText"], 
-                        attrs["actionButtonText"], attrs["actionFunction"], 
-                        attrs["cancelButtonText"], attrs["cancelFunction"], 
-                        scope, popupOptions);
-            });
-        }
-    };
+directives.directive('ngConfirm', function(Popup) {
+	return {
+		restrict: 'E',
+		link: function postLink(scope, element, attrs) {
+			// Could have custom or boostrap modal options here
+			var popupOptions = {};
+			element.bind("click", function() {
+				Popup.confirm(attrs["title"], attrs["actionText"],
+						attrs["actionButtonText"], attrs["actionFunction"],
+						attrs["cancelButtonText"], attrs["cancelFunction"],
+						scope, popupOptions);
+			});
+		}
+	};
 });
 
-directives.directive('ngAlert', function(PopupService) {
-    return {
-        restrict: 'E',
-        link: function postLink(scope, element, attrs) {
-            // Could have custom or boostrap modal options here
-            var popupOptions = {};
-            element.bind("click", function()
-            {
-                PopupService.alert(attrs["title"], attrs["text"], 
-                        attrs["buttonText"], attrs["alertFunction"], 
-                        scope, popupOptions);
-            });
-        }
-    };
+directives.directive('ngAlert', function(Popup) {
+	return {
+		restrict: 'E',
+		link: function postLink(scope, element, attrs) {
+			// Could have custom or boostrap modal options here
+			var popupOptions = {};
+			element.bind("click", function() {
+				Popup.alert(attrs["title"], attrs["text"],
+						attrs["buttonText"], attrs["alertFunction"],
+						scope, popupOptions);
+			});
+		}
+	};
 });
